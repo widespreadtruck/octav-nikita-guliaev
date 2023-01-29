b04c781c69f163981850a81cc575db6684a8bfb2
Nikita Guliaev <gulnikita63@gmail.com>
2023-01-29 15:06:08 +0300

    update styles for the warning

--- client/src/components/WarningBanner/WarningBanner.tsx
+++ client/src/components/WarningBanner/WarningBanner.tsx
@@ -12,11 +12,29 @@ const WarningBanner = ({ status }: { status: boolean }) => {
       className="fixed top-0 right-0 p-4"
       role="alert"
     >
-      <div className="bg-red-200 border-red-600 text-red-600 border-l-4 p-4">
+      {/* <div className="bg-red-200 border-red-600 text-red-600 border-l-4 p-4">
         <p className="font-bold">Warning</p>
         <p>
           Sorry... The Price data is currently unavailable for this asset. Try
           later.
+        </p>
+      </div> */}
+      <div className="flex bg-red-100 rounded-lg p-4 mb-4">
+        <svg
+          className="w-5 h-5 text-red-700"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <p className="ml-3 text-md text-red-700">
+          <span className="font-medium">Sorry... </span> The Price data is
+          currently unavailable for this asset. Try later.
         </p>
       </div>
     </animated.div>
@@ -24,16 +42,3 @@ const WarningBanner = ({ status }: { status: boolean }) => {
 }
 
 export default WarningBanner
-
-
-{/* <div
-      className={`fixed top-0 right-0 transition-all ease-in-out duration-700 transform ${
-        status ? "translate-x-0" : "translate-x-full"
-      }`}
-      role="alert"
-    >
-      <div className="bg-red-200 border-red-600 text-red-600 border-l-4 p-4">
-        <p className="font-bold">Warning</p>
-        <p>Sorry... The Price data is currently unavailable for this asset. Try later.</p>
-      </div>
-    </div> */}
