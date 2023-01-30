000ad243be79dbf238819b2ede29422ada8169e4
Nikita Guliaev <gulnikita63@gmail.com>
2023-01-30 12:11:05 +0300

    add lazy loading for imgs

---
+++ client/src/components/AssetSymbol/AssetSymbol.styles.ts
@@ -0,0 +1,10 @@
+
+import tw from "twin.macro"
+
+export const SymbolImg = tw.div`
+    flex flex-col items-center justify-center w-24 h-24 mr-4
+`
+
+export const Img = tw.img`
+    mx-auto object-cover rounded-full ml-0 mr-0 h-full w-full
+`
---
+++ client/src/components/AssetSymbol/AssetSymbol.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react"
+import * as S from "./AssetSymbol.styles"
+
+
+// lazy load the asset symbol image
+// show a skeleton loader while the img is loading
+const AssetSymbol = ({ assetData }: { assetData: any }) => {
+  const [imageLoaded, setImageLoaded] = useState(false)
+
+  useEffect(() => {
+    console.log("assetData", assetData)
+    const img = new Image()
+    img.src = assetData.assetsInfo[0].imgLarge
+    img.onload = () => {
+      setImageLoaded(true)
+    }
+  }, [assetData.assetsInfo[0].imgLarge])
+
+  return (
+    <div>
+      {imageLoaded ? (
+        <S.SymbolImg>
+          <S.Img alt="asset icon" src={assetData.assetsInfo[0].imgLarge} />
+        </S.SymbolImg>
+      ) : (
+        <S.SymbolImg>
+          <div className="mx-auto object-cover rounded-full ml-0 mr-0 h-full w-full bg-gray-300"></div>
+        </S.SymbolImg>
+      )}
+    </div>
+  )
+}
+
+export default AssetSymbol
